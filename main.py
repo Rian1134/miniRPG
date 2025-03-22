1dc1f67335a86493b462a0da84e5de183fff3287
Rian Japing Wijaya <rianjapingw@gmail.com>
2025-03-22 14:24:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -19,80 +19,25 @@ hpDua = 1000
 giliran1 = True
 giliran2 = False
 
+
 actionP1 = []
 actionP2 = []
 
 power = rd.randint(1,10)
 
-action = ["FireBall",
-          "TunderStrike",
-          "FrostBall",
-          "PowerPunch",
-          "FirePunch",
-          "Heal",
-          "Block",
-          "BlackHole",
-          "FullRecovery",
-          "InstantDead",
-          "Atomic"
-        ]
-persiapan = True
-while persiapan == True:
-  print("\nOffensive\n"
-        "0.FireBall \t= -30\n"
-        "1.TunderStrike \t= -20 \n"
-        "2.FrostBall \t= -20 \n"
-        "3.PowerPunch \t= -10 \n"
-        "4.FirePunch \t= -15\n"
-        "Defensive\n"
-        "5.Heal \t\t= +30\n"
-        "6.Block \t= 0\n"
-        "exclusive\n"
-        "7.BlackHole \t= -500\n"
-        "8.FullRecovery \t= +9999\n"
-        "9.InstantDead \t= -9999\n"
-        "10.Atomic \t= -9999\n")
-  
-  setup1 = 1
-  print("\npemain1: \n")
-  while setup1 <= 4:
-    pilihan1 = int(input("pilih action: "))
-    actionP1.append(action[pilihan1])
-    setup1 = setup1 + 1
-    
-  setup2 = 1
-  print("\npemain2: \n")
-  while setup2 <= 4:
-    pilihan2 = int(input("pilih action: "))
-    actionP2.append(action[pilihan2])
-    setup2 = setup2 + 1
-    persiapan = False
-    
-print(f"\nPemain1\t:{pemainSatu}\n"
-      f"hp\t:{hpSatu}\n"
-      f"action\t:{actionP1}\n"
-      
-      f"\nPemain2\t:{pemainDua}\n"
-      f"hp\t:{hpDua}\n"
-      f"action\t:{actionP2}\n"
-      )
 
-start = input("mulai(y/n): ")
-if start == 'y':
-  os.system('cls')
-  while hpSatu >= 0 or hpDua >= 0:
-    turn = 1
-    turn = turn + 1
-    print(f"turn: {turn}")
-    print(f"pemain 1: {hpSatu}")
-    print(f"pemain 2: {hpDua}")
-    while giliran1 == True:
-      print(f"{pemainSatu} apa yang ingin dilakukan?\n")
-      for x in actionP1:
-        print(f"hp: {hpSatu}")
-        print(actionP1.index, ".", x)
-        turnP1 = input("pilih action: ")
-        turnP1 = actionP1[turnP1]
-        
+def FireBall():
+    dmg = 30 * power
+    if giliran1:
+      hpSatu - dmg
+    if giliran2:
+      hpDua - dmg
+
+def TunderStrike():
+  dmg = 20 * power
+  if giliran1:
+      hpSatu - dmg
+  if giliran2:
+      hpDua - dmg
   
-      
+  
